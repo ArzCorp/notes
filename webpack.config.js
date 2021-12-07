f81c43aaee6eeed3c6c5b27588e3d20f3236bb91
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2021-12-07 14:27:50 -0600

    🎉 initial commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+	entry: path.resolve(__dirname, './src/index.js'),
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'bundle.js',
+	},
+	devServer: {
+		compress: true,
+		port: 3000,
+		static: {
+			directory: path.resolve(__dirname, './dist'),
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\html$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'html-loader',
+				},
+			},
+			{
+				test: /\.(css|scss)$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx'],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'public/index.html',
+			filename: './index.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+	],
+}
