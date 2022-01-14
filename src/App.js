9698f8ba328eee8069397a1da9801256fcadfa15
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-01-14 07:18:20 -0600

    💄 created logo component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Logo from './components/Logo'
 
 export default function App() {
 	return (
 		<div className="app">
-			<h1>React + Sass template</h1>
+			<Logo />
 		</div>
 	)
 }
---
+++ src/components/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const P = styled.p`
+	color: pink;
+	font-size: 20px;
+`
+
+export default function Logo() {
+	return (
+		<div>
+			<P>Logo</P>
+		</div>
+	)
+}
