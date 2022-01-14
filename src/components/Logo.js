510c48f966cc8f4d9d0f87f7c6932c1b92a62e0e
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-01-14 08:01:32 -0600

    💄 improved logo styles

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { DARK_COLOR } from '../assets/colors'
+import { BIG_TEXT, STRONG_FONT } from '../assets/fonts'
+
 const P = styled.p`
-	color: pink;
-	font-size: 20px;
+	color: ${DARK_COLOR};
+	font-size: ${BIG_TEXT};
+	font-family: ${STRONG_FONT};
+	font-weight: 700;
 `
 
 export default function Logo() {
 	return (
 		<div>
-			<P>Logo</P>
+			<P>Comment section</P>
 		</div>
 	)
 }
