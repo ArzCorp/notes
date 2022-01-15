3fb5f86f11109b7ae34f51cfbb8341e5f36ed1fc
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-01-14 22:44:39 -0600

    💄 added button component

---
+++ src/components/Button.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+import { DARK_COLOR, PRIMARY_LIGHT_COLOR } from '../assets/colors'
+import { NORMAL_TEXT } from '../assets/fonts'
+
+const ButtonStyles = styled.button`
+	width: 100%;
+	padding: 10px 20px;
+	color: ${DARK_COLOR};
+	border: 1px solid ${DARK_COLOR};
+	border-radius: 10px;
+	background-color: ${PRIMARY_LIGHT_COLOR};
+	font-size: ${NORMAL_TEXT};
+  font-weight: 700;
+	cursor: pointer;
+  transition: 0.2s ease-out;
+  will-change: auto;
+
+	&:hover {
+		color ${PRIMARY_LIGHT_COLOR};
+    background-color: ${DARK_COLOR};
+	}
+`
+
+export default function Button(props) {
+	return <ButtonStyles {...props} />
+}
