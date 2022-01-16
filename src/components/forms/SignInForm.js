8d412fd3b1d6b2f4f13e4d91544b1a0a92496bf3
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-01-15 23:06:51 -0600

    ✨ added image profile

--- src/components/forms/SignInForm.js
+++ src/components/forms/SignInForm.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 import styled from 'styled-components'
 
-import { SECONDARY_COLOR } from '../../assets/colors'
+import { DARK_COLOR, SECONDARY_COLOR } from '../../assets/colors'
 import { SMALL_TEXT } from '../../assets/fonts'
 
 import Button from '../Button'
@@ -16,7 +16,49 @@ const ErrorForm = styled.p`
 	margin-top: 10px;
 `
 
+const ImageContainer = styled.div`
+	width: 150px;
+	color: transparent;
+	overflow: hidden;
+	height: 150px;
+	position: relative;
+	border: 1px solid ${DARK_COLOR};
+	border-radius: 100%;
+	margin: 0px auto;
+	margin-bottom: 20px;
+`
+
+const InputFile = styled.input`
+	cursor: pointer;
+	position: relative;
+	z-index: 50;
+`
+
+const Icon = styled.i`
+	position: absolute;
+	z-index: 5;
+	top: calc(50% - 20px);
+	left: calc(50% - 20px);
+	font-size: 40px;
+	color: ${DARK_COLOR};
+`
+
+const UserImage = styled.img`
+	position: absolute;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	right: 0;
+	z-index: 10;
+	border-radius: 100%;
+	object-fit: cover;
+	width: 100%;
+	height: 100%;
+`
+
 export default function SignInForm() {
+	const file = useRef()
+	const [image, setImage] = useState('')
 	const { error, register } = useRegister({
 		onSubmit: () => {
 			handleReset()
@@ -33,7 +75,10 @@ export default function SignInForm() {
 		handleReset,
 	} = useFormik({
 		onSubmit: (data) => {
-			register(data)
+			register({
+				...data,
+				userImage: image,
+			})
 		},
 		initialValues: {
 			name: '',
@@ -52,8 +97,32 @@ export default function SignInForm() {
 			email: Yup.string().email('Correo no válido').required('Campo requerido'),
 		}),
 	})
+
+	const handleFile = (e) => {
+		e.preventDefault()
+		const dataFile = file.current.files[0]
+		var reader = new FileReader()
+
+		reader.onloadend = function () {
+			setImage(reader.result)
+		}
+
+		if (dataFile) return reader.readAsDataURL(dataFile)
+	}
+
 	return (
 		<form onSubmit={handleSubmit}>
+			<ImageContainer>
+				<InputFile
+					ref={file}
+					type="file"
+					name="userImage"
+					id="image"
+					onChange={handleFile}
+				/>
+				<Icon className="fas fa-camera" />
+				<UserImage src={image} alt="profile" />
+			</ImageContainer>
 			<TextInput
 				label="Nombre:"
 				name="name"
