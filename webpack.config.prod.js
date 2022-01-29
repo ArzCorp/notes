1d39259582a60b71f862d2cb63ff069a55563ecf
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-01-29 16:12:36 -0600

    🚀 adedd script to deploy

---
+++ webpack.config.prod.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+	entry: path.resolve(__dirname, './src/index.js'),
+	output: {
+		publicPath: '/',
+		path: path.resolve(__dirname, './dist'),
+		filename: 'bundle.js',
+	},
+	mode: 'production',
+	devtool: 'eval-source-map',
+	devServer: {
+		historyApiFallback: true,
+		port: 3000,
+		static: {
+			directory: path.resolve(__dirname, './dist'),
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\html$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'html-loader',
+				},
+			},
+			{
+				test: /\.(css)$/,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx'],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'public/index.html',
+			filename: './index.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+	],
+}
